c2a42d0156a812532ce5db4cbd737c71c3140d15
oconno95 <oconno95@pnw.edu>
2023-04-10 21:05:55 -0500

    Set up workspace for VSCode

---
+++ src/battleship/ComputerPlayer.java
@@ -0,0 +1,11 @@
+package battleship;
+
+public class ComputerPlayer extends Player {
+
+    @Override
+    public void selectMove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'selectMove'");
+    }
+    
+}
---
+++ src/battleship/Player.java
@@ -0,0 +1,11 @@
+package battleship;
+
+public abstract class Player {
+
+    public Player() {
+
+    }
+
+    public abstract void selectMove();
+    
+}
